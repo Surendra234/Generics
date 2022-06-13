40ef859d993698da8f5a64de834905d40eb24427
Surendra234 <surendramahawar873@gmail.com>
2022-06-13 16:19:17 +0530

    Find The Maximum Number

--- src/com/bridgelabz/FindMaximum.java
+++ src/com/bridgelabz/FindMaximum.java
@@ -2,15 +2,16 @@ package com.bridgelabz;
 
 public class FindMaximum{
 
-    public Integer maxNum(Integer x, Integer y, Integer z) {
+    public Float maxNum(Float x, Float y, Float z) {
 
-        Integer max = x;
+        Float max = x;
 
-        if(y.compareTo(max) > 0)
+        if(y.compareTo(max) > 0) {
             max = y;
-
-        if(z.compareTo(max) > 0)
+        }
+        if(z.compareTo(max) > 0) {
             max = z;
+        }
 
         return max;
     }
--- src/com/bridgelabz/Generics.java
+++ src/com/bridgelabz/Generics.java
@@ -6,13 +6,13 @@ public class Generics {
 
         FindMaximum findMaximum = new FindMaximum();
 
-        int testCase1 = findMaximum.maxNum(12,5,8);
+        float testCase1 = findMaximum.maxNum(12.2f,11.1f,10.0f);
         System.out.println("Maximum Number for Case-1 is : " +testCase1);
 
-        int testCase2 = findMaximum.maxNum(25,30,10);
+        float testCase2 = findMaximum.maxNum(15.5f,16.6f,14.4f);
         System.out.println("Maximum Number for Case-2 is : " +testCase2);
 
-        int testCase3 = findMaximum.maxNum(30,35,45);
+        float testCase3 = findMaximum.maxNum(23.3f,34.4f,56.6f);
         System.out.println("maximum Number for Case-3 is : " +testCase3);
     }
 }
